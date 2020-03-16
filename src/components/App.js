e55ff58d95ea35da557af65c5f7f8b1f80c74cde
Pierre Charles <p.charles0501@gmail.com>
2020-03-16 23:17:07 +0000

    Fixing netlify routing issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Home from './Home'
 import Login from './Login'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import '../stylesheets/App.scss'
 
 export default class App extends Component {
