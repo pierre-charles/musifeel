73af2e027382a87c298d96cfcca44b7c18918c54
Pierre Charles <40791587+Pierre-Charles@users.noreply.github.com>
2020-03-19 11:49:27 +0000

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,7 @@ export default class Login extends Component {
           <div className='container text-center text-white'>
             <h1 className='pb-3 header'>musifeel <Emoji emoji="musical-note"/></h1>
             <p className='text-center mx-5 mb-5'>Creates a Spotify playlist by identifying your mood.</p>
-            <a className='spotify-login' href={`https://accounts.spotify.com/authorize?client_id=5d549dda46474e9e8422bc9803497cb6&redirect_uri=http%3A%2F%2Fmusifeel.netlify.com%2Fsuggest&scope=user-top-read%20playlist-modify-public&response_type=token&state=123`}><i className='fab fa-spotify fa-lg pr-1'/> Login with Spotify</a>
+            <a className='spotify-login' href={`https://accounts.spotify.com/authorize?client_id=5d549dda46474e9e8422bc9803497cb6&redirect_uri=https%3A%2F%2Fmusifeel.netlify.com%2Fsuggest&scope=user-top-read%20playlist-modify-public&response_type=token&state=123`}><i className='fab fa-spotify fa-lg pr-1'/> Login with Spotify</a>
           </div>
         </div>
       )
