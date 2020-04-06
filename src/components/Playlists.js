6fae521ba6bd191cdeb16bc2baaa106277303894
Pierre Charles <p.charles0501@gmail.com>
2020-04-06 15:51:34 +0100

    Adding currently playing line in Playlist component

--- src/components/Playlists.js
+++ src/components/Playlists.js
@@ -21,6 +21,10 @@ export default class Playlists extends Component {
 
   componentDidMount() {
     this.getRecentTracks()
+    this.currentlyPlaying()
+    setInterval(this.currentlyPlaying, 1000)
+    setInterval(this.getRecentTracks, 5000)
+
   }
 
   getRecentTracks = async () => {
@@ -65,6 +69,24 @@ export default class Playlists extends Component {
     { this.state.features.map(feature => { console.log('Danceability: ', feature.danceability, '\n', 'Energy: ', feature.energy, '\n', 'Valence: ', feature.valence, '\n') }) }
   }
 
+  currentlyPlaying = async () => {
+    const apiCall = await fetch(`https://api.spotify.com/v1/me/player/currently-playing`, {
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('accessToken')
+      }
+    })
+    const response = await apiCall.json()
+    this.setState({
+      currentlyPlaying: {
+        status: response.is_playing,
+        name: response.item.name,
+        album: response.item.album.name,
+        href: response.context.external_urls.spotify
+      }
+    })
+  }
+
+
   render() {
     const mood = localStorage.getItem('mood')
     const emojis = {
@@ -79,8 +101,8 @@ export default class Playlists extends Component {
     return (
       <div className='container text-white' >
         <div className='my-5'>
-          <h1>Music for your mood <Emoji emoji='musical-note' /></h1>
-          <h2 className='h4'><span className='mr-2'>Mood: {mood}</span>
+          <h1 className='title'>Music for your mood <Emoji emoji='musical-note' /></h1>
+          <h2 className='mood h4 '><span className='mr-2'>Mood: {mood}</span>
             {mood === 'happy' && <Emoji emoji={emojis.happy} />}
             {mood === 'sad' && <Emoji emoji={emojis.sad} />}
             {mood === 'neutral' && <Emoji emoji={emojis.neutral} />}
@@ -89,10 +111,11 @@ export default class Playlists extends Component {
             {mood === 'fearful' && <Emoji emoji={emojis.fearful} />}
             {mood === 'disgusted' && <Emoji emoji={emojis.disgusted} />}
           </h2>
+          {this.state.currentlyPlaying && <h3 className='h6 current-playing'>Currently playing: <a href={this.state.currentlyPlaying.href} target='_blank' rel="noopener noreferrer">{this.state.currentlyPlaying.name} - {this.state.currentlyPlaying.album}</a></h3>}
         </div>
         <div className='p-1 fluid-container bg-white py-5 px-5 color-primary shadow'>
           <div className='text-center'>
-            <h1 className='h2 pb-3'>Your favourite tracks</h1>
+            <h1 className='favourite-track h2 pb-3'>Your favourite tracks</h1>
             <div className='row p-0 color-tertiary'>
               <div className='col-12'>
                 <p><button className={this.state.activeTab === 'short_term' ? 'button-range-active' : 'button-range'} onClick={() => { this.getSpotifyTracks(this.state.range.shortTerm) }}>Last Month</button></p>
@@ -104,7 +127,7 @@ export default class Playlists extends Component {
           <hr />
           <div className='mt-4 p-1 row container'>
             <div className='col-lg-8 col-md-8 col-sm-12 mb-5'>
-              <h1 className='h3'>{this.state.activeTab ? 'Playlist 1' : 'Recently played'} <Emoji emoji='musical-note' /></h1>
+              <h1 className={this.state.activeTab ? 'playlist h3' : 'recently-played h3'}>{this.state.activeTab ? 'Playlist 1' : 'Recently played'} <Emoji emoji='musical-note' /></h1>
               <hr />
               {
                 !this.state.activeTab && this.state.recent.map(music => {
