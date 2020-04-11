322da0d17f6bb658a1a413990fe89637b1351bc5
Pierre Charles <p.charles0501@gmail.com>
2020-04-11 21:10:00 +0100

    Fixing popup padding issues

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,12 +6,14 @@ import Emoji from 'react-emojis'
 const Popup = (props) => {
   return (
     <div className='popup'>
-      <div className='bg-white rounded m-5 p-5'>
-        <button className='close' onClick={props.closePopup}><i className='fas fa-times' /></button>
-        <p className='ml-2 h4 pb-3'><Emoji emoji="partying-face" /> Yay! Your playlist has been saved!</p>
-        <p>You can now find it on Spotify playlists, here's what to look for:</p>
-        <p className='pt-3'>Playlist name: {props.name}</p>
-        <a className='view-playlist mt-2' href={props.href}>View on Spotify</a>
+      <div className='bg-white rounded m-5'>
+        <button className='close p-2' onClick={props.closePopup}><i className='fas fa-times' /></button>
+        <div className='px-4 py-5'>
+          <p className='ml-2 h4 pb-3'><Emoji emoji="partying-face" /> Yay! Your playlist has been saved!</p>
+          <p>You can now find it on Spotify playlists, here's what to look for:</p>
+          <p className='pt-3'>Playlist name: {props.name}</p>
+          <a className='view-playlist mt-2' href={props.href}>View on Spotify</a>
+        </div>
       </div>
     </div>
   )
