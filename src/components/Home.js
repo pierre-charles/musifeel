f7b3b3b2f8524839508b16167c0ad22efeb143f7
Pierre Charles <p.charles0501@gmail.com>
2020-04-12 17:38:49 +0100

    Reverting getUSerMedia changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,23 +29,6 @@ export default class Home extends Component {
   }
 
   startVideo = () => {
-    if (navigator.mediaDevices === undefined) {
-      navigator.mediaDevices = {}
-    }
-
-    if (navigator.mediaDevices.getUserMedia === undefined) {
-      navigator.mediaDevices.getUserMedia = function (constraints) {
-        var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-        if (!getUserMedia) {
-          return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
-        }
-
-        return new Promise(function (resolve, reject) {
-          getUserMedia.call(navigator, constraints, resolve, reject);
-        });
-      }
-    }
     navigator.mediaDevices.getUserMedia({
       video: { facingMode: 'user' }
     }).then(stream => this.video.current.srcObject = stream)
