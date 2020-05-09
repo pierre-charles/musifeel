2a067c8c9d3e0bca297519b04df8c5be23ad0efd
Pierre Charles <p.charles0501@gmail.com>
2020-05-09 21:15:44 +0100

    Changing to use React hooks

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react'
+import React, { useRef } from 'react'
 import '../stylesheets/Tracks.scss'
-export default class Track extends Component {
-  constructor(props) {
-    super(props)
-    this.audio = React.createRef();
-  }
 
-  render() {
-    return (
-      <div className='track'>
-        <hr />
-        <div className='row pt-2'>
-          <div className='col-12 text-center'>
-            <audio ref={this.audio} type="audio/mp3" src={this.props.preview}></audio>
-            <img className='album-art mb-2' src={this.props.albumArt} alt={this.props.name} onMouseEnter={() => { this.audio.current.volume = 0.25; this.audio.current.play() }} onMouseLeave={() => { this.audio.current.pause() }} />
-          </div>
-          <div className='col-12 m-0 text-center column'>
-            <p className='h5 m-0 font-weight-bold'>{this.props.name}</p>
-            <p className='h6 m-0 color-secondary'>{this.props.albumName}</p>
-            <p className='h6 m-0 color-tertiary'>{this.props.artist}</p>
-          </div>
+export default function Track(props) {
+  const audio = useRef()
+
+  return (
+    <div className='track'>
+      <hr />
+      <div className='row pt-2'>
+        <div className='col-12 text-center'>
+          <audio ref={audio} type="audio/mp3" src={props.preview}></audio>
+          <img className='album-art mb-2' src={props.albumArt} alt={props.name} onMouseEnter={() => { audio.current.volume = 0.25; audio.current.play() }} onMouseLeave={() => { audio.current.pause() }} />
         </div>
-      </div >
-    )
-  }
+        <div className='col-12 m-0 text-center column'>
+          <p className='h5 m-0 font-weight-bold'>{props.name}</p>
+          <p className='h6 m-0 color-secondary'>{props.albumName}</p>
+          <p className='h6 m-0 color-tertiary'>{props.artist}</p>
+        </div>
+      </div>
+    </div >
+  )
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
 import '../stylesheets/Login.scss'
 import { Link } from 'react-router-dom'
 import Emoji from 'react-emojis'
 import '../stylesheets/Welcome.scss'
 
-export default class Welcome extends Component {
-  constructor(props) {
-    super(props)
-    const hash = window.location.hash.substr(1).split('&')
-    const hashDecompose = hash[0].split('=')
-    const access_token = hashDecompose[1]
-    localStorage.setItem('accessToken', access_token)
-    this.state = {
-      name: null
-    }
-  }
+export default function Welcome(props) {
+  const hash = window.location.hash.substr(1).split('&')
+  const hashDecompose = hash[0].split('=')
+  const access_token = hashDecompose[1]
+  localStorage.setItem('accessToken', access_token)
+  const [name, setName] = useState(null)
 
-  componentDidMount() {
-    this.getSpotifyUserDetails()
-  }
+  useEffect(() => {
+    getSpotifyUserDetails()
+  }, [])
 
-  getSpotifyUserDetails = async () => {
+  const getSpotifyUserDetails = async () => {
     const apiCall = await fetch(`https://api.spotify.com/v1/me/`, {
       headers: {
         'Authorization': 'Bearer ' + localStorage.getItem('accessToken')
@@ -28,37 +23,35 @@ export default class Welcome extends Component {
     })
     const response = await apiCall.json()
     localStorage.setItem('spotifyID', response.id)
-    this.setState({ name: response.display_name.split(" ") })
+    setName(response.display_name.split(" "))
   }
 
-  render() {
-    return (
-      <div className='container py-5'>
-        {this.state.name && <div className='text-center text-white'>
-          <div className='m-5'>
-            <h1 className='pb-3 welcome-title'><Emoji emoji="waving-hand" /> Hello {this.state.name[0]}, welcome to musifeel</h1>
-            <p className='mx-5 mb-5'>Just a reminder... This app uses your webcam to scan your face to detect your mood.</p>
-            <div className='row m-5'>
-              <div className='col-md-4 col-sm-12 pb-3'>
-                <h1><Emoji emoji="computer-mouse" /></h1>
-                <p className='pt-2'>Hover over a song to hear a preview</p>
-              </div>
-              <div className='col-md-4 col-sm-12 pb-3'>
-                <h1><Emoji emoji="backhand-index-pointing-up" /></h1>
-                <p className='pt-2'>Click on a song to find it on Spotify</p>
-              </div>
-              <div className='col-md-4 col-sm-12 pb-3'>
-                <h1><Emoji emoji="red-heart" /></h1>
-                <p className='pt-2'>Click on the hearts to save your playlists</p>
-              </div>
+  return (
+    <div className='container py-5'>
+      {name && <div className='text-center text-white'>
+        <div className='m-5'>
+          <h1 className='pb-3 welcome-title'><Emoji emoji="waving-hand" /> Hello {name[0]}, welcome to musifeel</h1>
+          <p className='mx-5 mb-5'>Just a reminder... This app uses your webcam to scan your face to detect your mood.</p>
+          <div className='row m-5'>
+            <div className='col-md-4 col-sm-12 pb-3'>
+              <h1><Emoji emoji="computer-mouse" /></h1>
+              <p className='pt-2'>Hover over a song to hear a preview</p>
             </div>
-            <Link to={{ pathname: `/scan` }}>
-              <button type='button' className='button-scan'>Scan my mood!</button>
-            </Link>
+            <div className='col-md-4 col-sm-12 pb-3'>
+              <h1><Emoji emoji="backhand-index-pointing-up" /></h1>
+              <p className='pt-2'>Click on a song to find it on Spotify</p>
+            </div>
+            <div className='col-md-4 col-sm-12 pb-3'>
+              <h1><Emoji emoji="red-heart" /></h1>
+              <p className='pt-2'>Click on the hearts to save your playlists</p>
+            </div>
           </div>
+          <Link to={{ pathname: `/scan` }}>
+            <button type='button' className='button-scan'>Scan my mood!</button>
+          </Link>
         </div>
-        }
       </div>
-    )
-  }
+      }
+    </div>
+  )
 }
